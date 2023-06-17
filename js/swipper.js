700750dc92b645d8268f10c3aa59e246ce14f4bb
SANDIXST <46962096+SANDIXST@users.noreply.github.com>
2023-06-17 22:29:37 +0700

    адаптив

--- js/swipper.js
+++ js/swipper.js
@@ -1,11 +1,17 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    grid: {
-      rows: 1,
+  slidesPerView: 1,
+  grid: {
+    rows: 1,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is <= 767px (mobile devices)
+    767: {
+      slidesPerView: 3,
     },
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  },
+});
